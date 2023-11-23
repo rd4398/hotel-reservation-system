0dd1661ad6340a990022639ac7f5b25f18a94f60
krithikaiyer99 <iyer.kr@northeastern.edu>
2023-11-23 16:14:23 -0500

    add view

---
+++ sql/views.sql
@@ -0,0 +1,25 @@
+CREATE VIEW highestRatedHotels AS
+SELECT
+    h.name AS hotel_name,
+    AVG(r.rating) AS average_rating
+FROM
+    hotel h
+JOIN
+    review r ON h.hotel_id = r.hotel_id
+GROUP BY
+    h.hotel_id
+ORDER BY
+    average_rating
+
+CREATE VIEW mostBookedActivities AS
+SELECT
+    a.name AS activity_name,
+    COUNT(ab.booking_id) AS times_booked
+FROM
+    activity a
+JOIN
+    activityBooking ab ON a.activity_id = ab.activity_id
+GROUP BY
+    a.activity_id;
+ORDER BY
+times_booked DESC
